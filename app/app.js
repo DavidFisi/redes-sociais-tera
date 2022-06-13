a036650a4d99cecd2616713a98162cb77187b481
DavidFisi <jrfisicaro@gmail.com>
2022-06-13 20:18:47 -0300

    commit on master

--- app/app.js
+++
@@ -1,6 +0,0 @@
-const menuIconBtn = document.querySelector("#btn");
-const sidebar = documento.querySelector(".sidebar");
-
-menuIconBtn.addEventListener("click", ()=>{
-  sidebar.classlist.toggle("open")
-})
