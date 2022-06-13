31072d43da9e8fa98d5890fdca55f47d7173315f
DavidFisi <jrfisicaro@gmail.com>
2022-06-12 23:06:09 -0300

    commit on master

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
-let btn = document.querySelector('#btn');
-let sidebar = document.querySelector('.sidebar');
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
 /*let searchBtn = document.querySelector('#bx-search');*/
 
-btn.onclick = function(){
-    sidebar.classList.toggle("active");
+btn.onclick = function () {
+  sidebar.classList.toggle("active");
 }
